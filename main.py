537d424968b8fbbf6a29e8b244330a7ea8d5918e
Braxton Holland <hollandb132@gmail.com>
2025-10-01 03:30:08 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -191,4 +191,6 @@ def process_image():
     return response
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))  # Railway provides this automatically
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
